3a5ba7bb8f339309d7055596a65c1a348df8cf43
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-10-06 15:40:39 +0200

    Step 2 completed

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,7 +1,19 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X\nO X O\nX X O");
+        Scanner scanner = new Scanner(System.in);
+        // taking input from user
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+        char[] symbols = input.toCharArray();
+        // printing result game field
+        System.out.println("---------");
+        System.out.printf("| %s %s %s |\n", symbols[1], symbols[2], symbols[3]);
+        System.out.printf("| %s %s %s |\n", symbols[4], symbols[5], symbols[6]);
+        System.out.printf("| %s %s %s |\n", symbols[7], symbols[8], symbols[9]);
+        System.out.println("---------");
     }
 }
